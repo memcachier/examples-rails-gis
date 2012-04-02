6b8102bf50392149f2141778a83f6a4635c9f472
Alex Loddengaard <alexloddengaard@gmail.com>
2012-04-02 10:59:55 -0700

    Empty rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MemcachierGis::Application.config.secret_token = 'bb3a968ac1dce72917b27863d9da4442270a7708c824db323eeedf6cf4351a55cbe1fce438d7be2b68a24676e6f415ef4c2d326ccaf0518adb623bf8cae75514'
