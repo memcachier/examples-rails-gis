3885ec5140bc23f026446e3f2fc62b26aec177f2
Alex Loddengaard <alexloddengaard@gmail.com>
2012-04-02 11:23:46 -0700

    Small JS improvement - a simple loading message.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,9 +1,11 @@
 $(function() {
   $("input[data-perform-lookup]").live("click", function() {
+    var coordinates = $("#coordinates");
+    coordinates.html("Working ...");
     $.get(
       "/lookup?location=" + $("#location").val(),
       function(data) {
-        $("#coordinates").html(data);
+        coordinates.html(data);
       }
     );
   });
